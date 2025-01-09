641de64254f49dd74475303c5c0a67aefb73992a
Alan Easow <alaneasow@gmail.com>
2025-01-09 16:48:05 +0530

    Added setup db script for automatically create db if db doesn't exist

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,11 @@
 set -e
 set -x
 
+# Create database if doesn't exist
+python setup_db.py
+
 # Run migrations
 alembic upgrade head
 
 # Start FastAPI application
-exec uvicorn app.main:app --host 0.0.0.0 --port 8000
+exec uvicorn app.main:app --host 0.0.0.0 --port 8000 --workers 2
